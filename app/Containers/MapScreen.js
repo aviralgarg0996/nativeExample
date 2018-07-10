3be18103da04a36da5be94fcd690af4f3433ed82
Kartik2811 <kartiktandon@innotical.com>
2018-07-10 18:30:27 +0530

    changes

--- app/Containers/MapScreen.js
+++ app/Containers/MapScreen.js
@@ -5,19 +5,46 @@ import { connect } from "react-redux";
 import MapView from 'react-native-maps';
 // create a component
 class MapScreen extends Component {
-   
+  state={
+    latitude:"",
+    longitude:"",
+    error:""
+  }
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+       (position) => {
+         console.log("wokeeey");
+         console.log(position);
+         this.setState({
+           latitude: position.coords.latitude,
+           longitude: position.coords.longitude,
+           error: null,
+         });
+       },
+       (error) => this.setState({ error: error.message }),
+       { enableHighAccuracy: false, timeout: 200000, maximumAge: 1000 },
+     );
+   }
+
     render() {
         return (
             <View style={styles.container}>
         <MapView
           style={styles.map}
           region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
+            latitude:-6.270565,
+            longitude:106.759550,
+            latitudeDelta: 1,
+            longitudeDelta: 1
           }}
-        ></MapView>
+        >
+          {!!this.state.latitude && !!this.state.longitude && 
+          <MapView.Marker
+         coordinate={{"latitude":this.state.latitude,
+         "longitude":this.state.longitude}}
+         title={"Your Location"}
+       />}
+        </MapView>
       </View>
           
         );
